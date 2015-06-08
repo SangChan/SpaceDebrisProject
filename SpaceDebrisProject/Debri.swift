d072578b35e82ec53cdc6827b59c48f58a0753c5
SangChan Lee <sangchan82@gmail.com>
2015-06-08 13:57:33 +0900

    create class

---
+++ SpaceDebrisProject/Debri.swift
@@ -0,0 +1,13 @@
+//
+//  Debri.swift
+//  SpaceDebrisProject
+//
+//  Created by SangChan on 2015. 6. 8..
+//  Copyright (c) 2015년 LeeSangchan. All rights reserved.
+//
+
+import SpriteKit
+
+class Debri: SKSpriteNode {
+   
+}

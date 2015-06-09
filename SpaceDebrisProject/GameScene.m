89841a15ebf9a8d0920ef096088bc96337b0f1c1
SangChan Lee <sangchan82@gmail.com>
2015-06-09 18:07:08 +0900

    remove code

--- SpaceDebrisProject/GameScene.m
+++ SpaceDebrisProject/GameScene.m
@@ -96,22 +96,4 @@
     
 }
      
-
-
--(SKTexture*)textureWithVerticalGradientofSize:(CGSize)size topColor:(CIColor*)topColor bottomColor:(CIColor*)bottomColor
-{
-    CIContext *coreImageContext = [CIContext contextWithOptions:@{kCIContextUseSoftwareRenderer : @(NO)}];
-    CIFilter *gradientFilter = [CIFilter filterWithName:@"CILinearGradient"];
-    [gradientFilter setDefaults];
-    CIVector *startVector = [CIVector vectorWithX:size.width/2 Y:0];
-    CIVector *endVector = [CIVector vectorWithX:size.width/2 Y:size.height];
-    [gradientFilter setValue:startVector forKey:@"inputPoint0"];
-    [gradientFilter setValue:endVector forKey:@"inputPoint1"];
-    [gradientFilter setValue:bottomColor forKey:@"inputColor0"];
-    [gradientFilter setValue:topColor forKey:@"inputColor1"];
-    CGImageRef cgimg = [coreImageContext createCGImage:[gradientFilter outputImage]
-                                              fromRect:CGRectMake(0, 0, size.width, size.height)];
-    return [SKTexture textureWithImage:[UIImage imageWithCGImage:cgimg]];
-}
-
 @end

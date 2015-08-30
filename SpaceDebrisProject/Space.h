ed97c999b44060a098b21cc006db6be36c38de87
SangChan Lee <sangchan82@gmail.com>
2015-08-30 22:01:45 +0900

    make Space object

---
+++ SpaceDebrisProject/Space.h
@@ -0,0 +1,13 @@
+//
+//  Space.h
+//  SpaceDebrisProject
+//
+//  Created by LeeSangchan on 2015. 8. 30..
+//  Copyright (c) 2015년 LeeSangchan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Space : NSObject
+
+@end
---
+++ SpaceDebrisProject/Space.m
@@ -0,0 +1,13 @@
+//
+//  Space.m
+//  SpaceDebrisProject
+//
+//  Created by LeeSangchan on 2015. 8. 30..
+//  Copyright (c) 2015년 LeeSangchan. All rights reserved.
+//
+
+#import "Space.h"
+
+@implementation Space
+
+@end
